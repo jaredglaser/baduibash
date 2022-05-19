54b1f3cefa602fb070e32b31de5ac4f54bd23134
Jared Glaser <Jared@glaserfamily.net>
2022-05-18 21:52:39 -0400

    fixed all parts that use the slots function

--- Starter Resume Code/Resume/Resume.js
+++ Starter Resume Code/Resume/Resume.js
@@ -1,94 +1,173 @@
 
-const listOfUIElements = ["slider","map","resume-name-box","resume-dob-box","resume-email-box","resume-phone-box","map-box",];
+const listOfUIElements = ["slider", "map", "resume-name-box", "resume-dob-box", "resume-email-box", "resume-phone-box",];
 var map;
 var currentID = 'resume-name-box';
+var slot1 = false;
+var intervalId;
 
-function toggle_visibility(id) {
-    showUIElement(id);
-}
+
 
 function showValue(newValue) {
-    newValue = newValue.padStart(10, "0");
-    newValue = "(" + newValue.slice(0, 3) + ") " + newValue.slice(3, 6) + "-" + newValue.slice(6, 10);
-    document.getElementById("phone").value = newValue;
+  newValue = newValue.padStart(10, "0");
+  newValue = "(" + newValue.slice(0, 3) + ") " + newValue.slice(3, 6) + "-" + newValue.slice(6, 10);
+  document.getElementById("phone").value = newValue;
 }
 
 function changeJobTitle(job) {
-    document.getElementById("job-title").value = job;
+  document.getElementById("job-title").value = job;
 }
 
-function showUIElement(toShow){
-    //first hide all elements
-    hideUIElements();
-    $('#input-box').show();
-    $('#'+toShow).show();
-    map.updateSize();
-    return false;
+function runSlot(id) {
+  var i = 0;
+  var max = 26;
+  if(id == 'job-description' || id == 'email' || id == 'description' ){
+    max = 126;
+  }
+  var oldValue = $('#' + id).val();
+  intervalId = window.setInterval(function () {
+    $('#' + id).val(oldValue + String.fromCharCode(97 + i++));
+    if (i == max) {
+      i = 0;
+    }
+  }, 70);
+
+
+
 }
 
-function hideUIElements(){
-  for(const element of listOfUIElements){
-    $('#'+element).hide();
+function showUIElement(toShow) {
+  //first hide all elements
+  hideUIElements();
+  $('#input-box').show();
+  $('#' + toShow).show();
+  map.updateSize();
+  return false;
 }
+
+function hideUIElements() {
+  for (const element of listOfUIElements) {
+    $('#' + element).hide();
+  }
 }
 
 function reverseGeocode(coords) {
-    console.log(coords);
-    fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + coords[0] + '&lat=' + coords[1])
-      .then(function(response) {
-             return response.json();
-         }).then(function(json) {
-          $('#addressinput').prop('disabled',false);
-                $('#addressinput').val(json.display_name);
-                $('#addressinput').prop('disabled',true);
-         });
- }
- $(document).ready(function () {
+  console.log(coords);
+  fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + coords[0] + '&lat=' + coords[1])
+    .then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      
+      $('#addressinput').val(json.display_name);
+     
+    });
+}
+$(document).ready(function () {
   $('#input-box').hide();
-  $('#addressinput').prop('disabled',true);
-  hideUIElements();
-    map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
-        })
-      });
-      map.on('singleclick', function(evt) {
-        var coordinate = evt.coordinate;
-        console.log(ol.coordinate);
-        var hdms = ol.proj.toLonLat(
-            coordinate);
-    
-        console.log(hdms);
-        reverseGeocode(hdms);
-      });
-      $('#setAddress').on('click', function(evt) {
 
-        showUIElement('map-box');
-        evt.preventDefault();
-        
+  hideUIElements();
+  map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
       })
-      $('addressinput').on('click', function(evt){
-        evt.preventDefault();
-      })
-     
-     
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+  });
+  map.on('singleclick', function (evt) {
+    var coordinate = evt.coordinate;
+    console.log(ol.coordinate);
+    var hdms = ol.proj.toLonLat(
+      coordinate);
+
+    console.log(hdms);
+    reverseGeocode(hdms);
+  });
+  $('#setAddress').on('click', function (evt) {
+
+    showUIElement('map-box');
+    showUIElement('map');
+    evt.preventDefault();
+
+  });
+  //$('addressinput').on('click', function (evt) {
+ //   evt.preventDefault();
+ // });
+  $('#phone').on('click', function(evt){
+    showUIElement('resume-phone-box');
     
- /*$(map).on('click', function (evt) {
-     console.log(map);
-     console.log(ol);
-    console.log(evt.target.valueAsNumber);
-    console.log(evt.target);
-    var coord = ol.proj.toLonLat(evt.coordinate);
-    console.log(coord);
-    reverseGeocode(coord);
-  });*/
+  })
+  $('#slot-first-name').on('click', function (evt) {
+
+    slotbuttonMethod('slot-first-name','first-name');
+
+    evt.preventDefault();
+
+  });
+
+  $('#slot-last-name').on('click', function (evt) {
+
+    slotbuttonMethod('slot-last-name','last-name');
+
+    evt.preventDefault();
+
+  });
+
+  $('#slot-email').on('click', function (evt) {
+
+    slotbuttonMethod('slot-email','email');
+
+    evt.preventDefault();
+
+  });
+
+  $('#slot-job-description').on('click', function (evt) {
+
+    slotbuttonMethod('slot-job-description','job-description');
+
+    evt.preventDefault();
+
+  });
+
+  $('#slot-description').on('click', function (evt) {
+
+    slotbuttonMethod('slot-description','description');
+
+    evt.preventDefault();
+
+  });
+
+
+  function slotbuttonMethod(id1,id2){
+    if ($('#'+id1).text() == 'Next') {
+      runSlot(id2);
+      $('#'+id1).text('Stop');
+
+    }
+    else {
+      clearInterval(intervalId);
+      $(('#'+id1)).text('Next');
+    }
+    
+
+  }
+
+  
+
+
+
+  /*$(map).on('click', function (evt) {
+      console.log(map);
+      console.log(ol);
+     console.log(evt.target.valueAsNumber);
+     console.log(evt.target);
+     var coord = ol.proj.toLonLat(evt.coordinate);
+     console.log(coord);
+     reverseGeocode(coord);
+   });*/
 });
 
 
